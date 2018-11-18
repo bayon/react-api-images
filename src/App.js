c76088302d284652afc619c2ad8124b3873476b6
Bayon Forte <bayon@forteworks.com>
2018-11-17 21:04:38 -0500

    pictures worked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      pictures: [],
+    }
+  }
+  /*
+https://jsonplaceholder.typicode.com/posts
+  */
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=100')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      let pictures = data.results.map((pic) => {
+        return(
+          <div key={pic.results}>
+            <img src={pic.picture.medium} />
+          </div>
+        )
+      })
+      this.setState({pictures: pictures});
+      console.log("state", this.state.pictures);
+    })
+  }
+   
+  
   render() {
+     
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          {this.state.pictures}
+        </div>
       </div>
     );
   }

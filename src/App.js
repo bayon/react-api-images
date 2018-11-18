dfeccef071745f4936bdfbda92ad81d41ab656b9
Bayon Forte <bayon@forteworks.com>
2018-11-18 07:51:53 -0500

    added forteworks api successfully

--- src/App.js
+++ src/App.js
@@ -5,13 +5,51 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      pictures: [],
+      contactsData: [],
     }
   }
  /*
+ https://randomuser.me/api/?results=100
 <img src={pic.picture.medium} alt={pic.name.first} className="list-img" />
+//---------------------
+http://www.forteworks.com/api/simple-api.php/contacts/
  */
   componentDidMount() {
+    fetch('http://www.forteworks.com/api/simple-api.php/contacts')
+    .then(results => {
+      return results.json();
+       
+    }).then(data => {
+      console.log(data);
+      
+      let contactsData = data.map((contact) => {
+        return(
+          <li key={new Date() * Math.random()*3 } className="individual-img" >
+            
+            <div className="list-city">{contact.contact_name}</div>
+          </li>
+        )
+      })
+      this.setState({contactsData: contactsData});
+      console.log("state", this.state.contactsData);
+    })
+  }
+   
+  
+  render() {
+     
+    return (
+      <div className="App">
+        <div className="listContainer">
+        <ul>
+         {this.state.contactsData}
+         </ul>
+        </div>
+      </div>
+    );
+  }
+  /*
+componentDidMount() {
     fetch('https://randomuser.me/api/?results=100')
     .then(results => {
       return results.json();
@@ -42,6 +80,7 @@ class App extends Component {
       </div>
     );
   }
+  */
 }
 
 export default App;
